2adaf98a8f90b44f1f22203d22fa5d37e16921ae
Paulina Witkowska <paul.witk@gmail.com>
2018-01-21 20:03:11 +0100

    Initial Commit

---
+++ library/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  library
+//
+//  Created by Paulina Witkowska on 21.01.2018.
+//  Copyright © 2018 Paulina Witkowska. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
